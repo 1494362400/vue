0639097a710aec2aba741992f4d4c1c176352701
HXJ <1494362400@qq.com>
2021-07-10 16:22:57 +0800

    2021/07/10 16：22修改

---
+++ Vue.config.js
@@ -0,0 +1,12 @@
+// vue.config.js
+const path = require('path')
+
+module.exports = {
+    pluginOptions: {
+        'style-resources-loader': {
+            preProcessor: 'less',
+        }
+    },
+}
+
+
